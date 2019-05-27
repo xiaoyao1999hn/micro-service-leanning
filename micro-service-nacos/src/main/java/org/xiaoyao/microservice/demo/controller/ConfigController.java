b03040cb0675f96680b437c24df87130ad4f9872
chengjie <chengjie2@globalegrow.com>
2019-05-27 09:08:12 +0800

    Nacos配置中心

--- micro-service-nacos/src/main/java/org/xiaoyao/microservice/demo/controller/ConfigController.java
+++ micro-service-nacos/src/main/java/org/xiaoyao/microservice/demo/controller/ConfigController.java
@@ -1,11 +1,11 @@
 package org.xiaoyao.microservice.demo.controller;
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 /**
+ * 添加RefreshScope后，如果nacos配置中心的信息发生变化会主动推送到我们的服务
+ * 如下面的例子，如果修改了nacos配置中的username，我们访问/demo/getUserName接口得到的username也会变
  * @author ChengJie
  * @desciption
  * @date 2019/5/26 17:49
@@ -20,8 +20,6 @@ public class ConfigController {
 
     @RequestMapping("/getUserName")
     public String getUserName(){
-
         return userName;
     }
-
 }
